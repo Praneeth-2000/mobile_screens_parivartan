1a2a311df60ecfc8a5240dfe644dee01315e62bd
Tharunteja <pellakurutharunteja@gmail.com>
2026-01-08 16:41:30 +0530

    last fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import HomeContainer from './components/HomeContainer/HomeContainer';
 import PortfolioContainer from './components/PortfolioContainer/PortfolioContainer';
 import ServicesShowcase from './components/PortfolioContainer/ServicesShowcase';
-import { WhyChooseUs } from './components/WhyChooseUs/WhyChooseUs';
-import { FAQ } from './components/FAQ/FAQ';
-import Testimonials from './components/Testimonials/Testimonials';
-import ContactFooter from './components/ContactFooter/ContactFooter';
+// import { WhyChooseUs } from './components/WhyChooseUs/WhyChooseUs';
+// import { FAQ } from './components/FAQ/FAQ';
+// import Testimonials from './components/Testimonials/Testimonials';
+// import ContactFooter from './components/ContactFooter/ContactFooter';
 // import BottomNav from './components/BottomNav/BottomNav';
 
 function App() {
@@ -14,10 +14,10 @@ function App() {
       <HomeContainer />
       <PortfolioContainer />
       <ServicesShowcase />
-      <WhyChooseUs />
-      <FAQ />
-      <Testimonials />
-      <ContactFooter />
+      {/* <WhyChooseUs /> */}
+      {/* <FAQ /> */}
+      {/* <Testimonials /> */}
+      {/* <ContactFooter /> */}
       {/* <BottomNav /> */}
     </div>
   );
